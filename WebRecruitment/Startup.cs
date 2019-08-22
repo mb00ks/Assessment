4ec4b1105a8d37909ef222227940f61d00734014
mboko <mboko.end.on@gmail.com>
2019-08-22 23:37:38 +0700

    update

--- WebRecruitment/Startup.cs
+++ WebRecruitment/Startup.cs
@@ -80,6 +80,7 @@ namespace WebRecruitment
                 options.Password.RequiredLength = 6;
                 options.Password.RequiredUniqueChars = 6;
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
